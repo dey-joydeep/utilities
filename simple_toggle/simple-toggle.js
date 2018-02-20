87e9c4d763c76bca362ed52cfd6830eb58132028
Joydeep Dey <dey.joydeep.cse@gmail.com>
2018-02-20 18:25:23 +0900

    toggle-switch element reduced to 1 from 2

--- simple_toggle/simple-toggle.js
+++ simple_toggle/simple-toggle.js
@@ -2,12 +2,20 @@
  * Simple toggle switch using jQuery. Copyright ©Joydeep Dey, 2018
  */
 
-var _groupCnt = 0;
 const
 SHAPE = Object.freeze({
-	SQUARE : 1,
-	ROUND : 0
+	BOX : 'BOX',
+	OVAL : 'OVL'
 });
+
+const
+size = Object.freeze({
+	XL : 2,
+	L : 1.6,
+	M : 1.3,
+	S : 1
+});
+
 const
 INIT_PARAM = {
 	onText : 'On',
@@ -19,7 +27,7 @@ INIT_PARAM = {
 	labelSize : '',
 	spanSize : '',
 	padding : 0,
-	shape : SHAPE.SQUARE,
+	shape : '',
 	changeColor : true
 }
 
@@ -28,25 +36,24 @@ $.fn.initToggle = function(param) {
 	TOGGLE.filterParams(param);
 
 	$.each(this, function(idx, elem) {
-		var sDiv = TOGGLE.createSwitches($(elem));
-		$(elem).appendTo(sDiv);
-		TOGGLE.handleSwitchSize(sDiv);
-		TOGGLE.handleSwitchColor();
+		var sDiv = TOGGLE.create($(elem));
+		TOGGLE.update($(this));
+		TOGGLE.handleSize(sDiv);
 
 		sDiv.find('.switch').click(function() {
 			sDiv.find('input[type=checkbox]').click();
 		});
 
 		$(elem).click(function(e) {
-			sDiv.find('.switch').toggle();
-			e.originalEvent.stopImmediatePropagation;
+			TOGGLE.update($(this));
 		});
 	});
-	_groupCnt++;
 }
 
 $.fn.destroyToggle = function() {
+	$.each(this, function(idx, elem) {
 
+	});
 }
 
 var TOGGLE = {
@@ -57,24 +64,19 @@ var TOGGLE = {
 			INIT_PARAM.off = this.checkParams(userParam.off) ? userParam.off
 					: 'Off';
 			INIT_PARAM.onClass = this.checkParams(userParam.onClass) ? userParam.onstyle
-					: 'switch-basic';
+					: 'sw-on';
 			INIT_PARAM.offClass = this.checkParams(userParam.offClass) ? userParam.offstyle
-					: '';
+					: 'sw-off';
 			INIT_PARAM.labelSize = this.checkParams(userParam.size) ? userParam.size
 					: '';
+			INIT_PARAM.shape = this.checkParams(userParam.shape) ? userParam.shape
+					: SHAPE.SQUARE;
 
 			if (this.checkParams(userParam.onStyle))
 				INIT_PARAM.onStyle = userParam.onstyle;
 			if (this.checkParams(userParam.offStyle))
 				INIT_PARAM.offStyle = userParam.offstyle;
 		}
-
-		var size = {
-			XL : 45,
-			L : 50,
-			M : 20,
-			S : 16
-		};
 
 		INIT_PARAM.onStyle = INIT_PARAM.onStyle.toLowerCase();
 		INIT_PARAM.offStyle = INIT_PARAM.offStyle.toLowerCase();
@@ -105,71 +107,77 @@ var TOGGLE = {
 		INIT_PARAM.padding = INIT_PARAM.padding;
 	},
 
-	createSwitches : function(target) {
-		var onLabel = $('<label class="switch switch-on"></label>');
-		var offLabel = $('<label class="switch switch-off"></label>');
+	create : function(target) {
+		var toggle = $('<label class="switch">&nbsp;</label>');
 
-		onLabel.text(INIT_PARAM.on);
-		offLabel.text(INIT_PARAM.off);
-
-		onLabel.addClass(INIT_PARAM.onClass);
-		offLabel.addClass(INIT_PARAM.offClass);
-
-		onLabel.addClass(INIT_PARAM.labelSize);
-		offLabel.addClass(INIT_PARAM.labelSize);
+		toggle.addClass(INIT_PARAM.onClass);
+		toggle.addClass(INIT_PARAM.labelSize);
 
 		var sDiv = $('<div class="switch-div"></div>').insertAfter(target);
-		sDiv.append(onLabel);
-		onLabel.append('<span class="toggle t-on"></span>');
-		sDiv.append(offLabel);
-		offLabel.append('<span class="toggle t-off"></span>');
+		sDiv.append(toggle);
+		toggle.append('<span class="toggle"></span>');
 
-		sDiv.find('.toggle').addClass(INIT_PARAM.spanSize);
+		var tElem = sDiv.find('.toggle');
+		tElem.addClass(INIT_PARAM.spanSize);
 
-		if (target.is(':checked')) {
-			offLabel.hide();
+		if (INIT_PARAM.shape === SHAPE.OVAL) {
+			toggle.addClass('switch-oval');
+			tElem.addClass('toggle-round');
 		} else {
-			onLabel.hide();
+			toggle.addClass('switch-square');
+			tElem.addClass('toggle-square');
 		}
+
+		sDiv.prepend(target);
 
 		return sDiv;
 	},
 
-	handleSwitchSize : function(parent) {
-		parent.find('.switch-off').css('padding-left', INIT_PARAM.padding + '%');
-		parent.find('.switch-on').css('padding-right', INIT_PARAM.padding + '%');
+	update : function(target) {
+		var label = target.next();
+		var span = label.children();
+		if (target.is(':checked')) {
+
+			this.addText(label, INIT_PARAM.on);
+			if (label.hasClass(INIT_PARAM.offClass))
+				label.removeClass(INIT_PARAM.offClass);
+			label.addClass(INIT_PARAM.onClass);
+
+			if (span.hasClass('t-off'))
+				span.removeClass('t-off');
+			span.addClass('t-on');
+			label.css('padding-left', '');
+			label.css('padding-right', INIT_PARAM.padding + '%');
+		} else {
+			this.addText(label, INIT_PARAM.off);
+			if (label.hasClass(INIT_PARAM.onClass))
+				label.removeClass(INIT_PARAM.onClass);
+			label.addClass(INIT_PARAM.offClass);
+
+			if (span.hasClass('t-on'))
+				span.removeClass('t-on');
+			span.addClass('t-off');
+			label.css('padding-right', '');
+			label.css('padding-left', INIT_PARAM.padding + '%');
+		}
 	},
 
-	handleSwitchColor : function() {
-		$('.switch-off').find('.t-off').css('background-color',
-				COLOR_HANDLER.getTextColor($('.switch-off')));
-		$('.switch-on').find('.t-on').css('background-color',
-				COLOR_HANDLER.getTextColor($('.switch-on')));
+	remove : function(target) {
+
 	},
+
+	handleSize : function(parent) {
+		// parent.find('.switch').css('padding-right', INIT_PARAM.padding +
+		// '%');
+	},
+
 	checkParams : function(param) {
 		return ((param !== undefined) && (param != null) && (param.trim().length > 0));
-	}
-}
-var COLOR_HANDLER = {
-	componentToHex : function(c) {
-		var hex = parseInt(c).toString(16);
-		return hex.length == 1 ? "0" + hex : hex;
 	},
 
-	rgbToHex : function(rgbStr) {
-		rgb = rgbStr.replace(new RegExp(/^rgb.*\(/, 'g'), '').replace(')', '')
-				.split(',');
-		return "#" + this.componentToHex(rgb[0]) + this.componentToHex(rgb[1])
-				+ this.componentToHex(rgb[2]);
-	},
-
-	getBGColor : function(selector) {
-		var bg = $(selector).css('background-color');
-		return this.rgbToHex(bg);
-	},
-
-	getTextColor : function(selector) {
-		var txt = $(selector).css('color');
-		return this.rgbToHex(txt);
+	addText : function(target, text) {
+		var textNode = target[0].childNodes[0];
+		var data = textNode.data;
+		textNode.replaceData(0, data.length, text);
 	}
 }
